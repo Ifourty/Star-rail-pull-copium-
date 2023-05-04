31dd023b8f2b0c12fea5cf6489e013d6817b3d75
Enzo chiattella <enzo.chiattella@gmal.com>
2023-05-04 09:47:44 +0200

    Opti code + Bannerchoice: button-> comboBox

--- Class/Visual/WindowsBanners.java
+++ Class/Visual/WindowsBanners.java
@@ -8,6 +8,8 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.File;
 import java.util.ArrayList;
 
@@ -17,6 +19,7 @@ import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -26,25 +29,25 @@ import Class.Gacha.Banner;
 import Class.Gacha.Gacha;
 
 public class WindowsBanners extends JFrame implements ActionListener {
-    private String[] seeleLimited_5 = { "Seele" };
-    private String[] seeleLimited_4 = { "Hook", "March_7th", "Herta" };
-    private Banner bannerSeelePull = new Banner(seeleLimited_5, seeleLimited_4);
+    private static final String[] seeleLimited_5 = { "Seele" };
+    private static final String[] seeleLimited_4 = { "Hook", "March_7th", "Herta" };
+    private static final Banner bannerSeelePull = new Banner(seeleLimited_5, seeleLimited_4);
 
-    private String[] kafkaLimited_5 = { "Kafka" };
-    private String[] kafkaLimited_4 = { "Herta", "Qingque", "Tingyun" };
-    private Banner bannerKafkaPull = new Banner(kafkaLimited_5, kafkaLimited_4);
+    private static final String[] kafkaLimited_5 = { "Kafka" };
+    private static final String[] kafkaLimited_4 = { "Herta", "Qingque", "Tingyun" };
+    private static final Banner bannerKafkaPull = new Banner(kafkaLimited_5, kafkaLimited_4);
 
-    private String[] fuXuanLimited_5 = { "Fu_Xuan" };
-    private String[] fuXuanLimited_4 = { "Sushang", "Qingque", "Arlan" };
-    private Banner bannerFuXuanPull = new Banner(fuXuanLimited_5, fuXuanLimited_4);
+    private static final String[] fuXuanLimited_5 = { "Fu_Xuan" };
+    private static final String[] fuXuanLimited_4 = { "Sushang", "Qingque", "Arlan" };
+    private static final Banner bannerFuXuanPull = new Banner(fuXuanLimited_5, fuXuanLimited_4);
 
-    private String[] bronyaLimited_5 = { "Bronya" };
-    private String[] bronyaLimited_4 = { "Hook", "March_7th", "Herta" };
-    private Banner bannerBronyaPull = new Banner(bronyaLimited_5, bronyaLimited_4);
+    private static final String[] bronyaLimited_5 = { "Bronya" };
+    private static final String[] bronyaLimited_4 = { "Hook", "March_7th", "Herta" };
+    private static final Banner bannerBronyaPull = new Banner(bronyaLimited_5, bronyaLimited_4);
 
-    private String[] bladeLimited_5 = { "Blade" };
-    private String[] bladeLimited_4 = { "Hook", "March_7th", "Herta" };
-    private Banner bannerBladePull = new Banner(bladeLimited_5, bladeLimited_4);
+    private static final String[] bladeLimited_5 = { "Blade" };
+    private static final String[] bladeLimited_4 = { "Hook", "March_7th", "Herta" };
+    private static final Banner bannerBladePull = new Banner(bladeLimited_5, bladeLimited_4);
 
     private int actualConfig = 0;
 
@@ -55,12 +58,12 @@ public class WindowsBanners extends JFrame implements ActionListener {
     private JButton one;
     private JButton ten;
     private JButton back;
-    private JButton next;
-    private JButton previous;
 
     private ImageIcon bannerSeele;
     private Image tempo;
     private JLabel banner;
+
+    private JComboBox<String> bannerComboBox;
 
     private ArrayList<String> result;
 
@@ -70,12 +73,13 @@ public class WindowsBanners extends JFrame implements ActionListener {
     private boolean wait = false;
 
     private ArrayList<String> soundList = new ArrayList<>();
+    private ArrayList<String> bannerPromoListe = new ArrayList<>();
 
     private String resultat;
 
     public WindowsBanners() {
+        initSoundAndImage();
         initComponents();
-        initSound();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
         Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
@@ -87,11 +91,17 @@ public class WindowsBanners extends JFrame implements ActionListener {
         setIconImage(bronya);
     }
 
-    private void initSound() {
+    private void initSoundAndImage() {
         soundList.add("Seele");
         soundList.add("Herta");
         soundList.add("Bronya");
         soundList.add("Kafka");
+
+        bannerPromoListe.add("Seele");
+        bannerPromoListe.add("Bronya");
+        bannerPromoListe.add("Kafka");
+        bannerPromoListe.add("Fu_Xuan");
+        bannerPromoListe.add("Blade");
     }
 
     private void initComponents() {
@@ -111,18 +121,23 @@ public class WindowsBanners extends JFrame implements ActionListener {
         panelButton = new JPanel();
         panelButton.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        previous = new JButton("Previous");
-        previous.addActionListener(this);
         one = new JButton("Pull x1");
         one.addActionListener(this);
         ten = new JButton("Pull x10");
         ten.addActionListener(this);
-        next = new JButton("Next");
-        next.addActionListener(this);
+        bannerComboBox = new JComboBox<>();
+        for (String ban : bannerPromoListe) {
+            bannerComboBox.addItem(ban);
+        }
+        bannerComboBox.addItemListener(new ItemListener() {
+            public void itemStateChanged(ItemEvent e) {
+                reload(e.getItem().toString());
+            }
+        });
 
         panelButton.add(one);
         panelButton.add(ten);
-        panelButton.add(next);
+        panelButton.add(bannerComboBox);
         panelButton.setBorder(BorderFactory.createLineBorder(Color.gray, 5));
 
         /* Add final */
@@ -139,15 +154,9 @@ public class WindowsBanners extends JFrame implements ActionListener {
         if (!wait) {
             if (e.getSource() == one) {
                 switch (actualConfig) {
-                    case 0:
-                        resultat = Gacha.pull(bannerSeelePull);
-                        break;
-                    case 1:
-                        resultat = Gacha.pull(bannerKafkaPull);
-                        break;
-                    case 2:
-                        resultat = Gacha.pull(bannerFuXuanPull);
-                        break;
+                    case 0 -> resultat = Gacha.pull(bannerSeelePull);
+                    case 1 -> resultat = Gacha.pull(bannerKafkaPull);
+                    case 2 -> resultat = Gacha.pull(bannerFuXuanPull);
                 }
                 panelButton.removeAll();
                 panelButton.add(one);
@@ -169,21 +178,11 @@ public class WindowsBanners extends JFrame implements ActionListener {
             if (e.getSource() == ten) {
                 wait = true;
                 switch (actualConfig) {
-                    case 0:
-                        result = Gacha.multiPull(bannerSeelePull);
-                        break;
-                    case 1:
-                        result = Gacha.multiPull(bannerBronyaPull);
-                        break;
-                    case 2:
-                        result = Gacha.multiPull(bannerKafkaPull);
-                        break;
-                    case 3:
-                        result = Gacha.multiPull(bannerFuXuanPull);
-                        break;
-                    case 4:
-                        result = Gacha.multiPull(bannerBladePull);
-                        break;
+                    case 0 -> result = Gacha.multiPull(bannerSeelePull);
+                    case 1 -> result = Gacha.multiPull(bannerBronyaPull);
+                    case 2 -> result = Gacha.multiPull(bannerKafkaPull);
+                    case 3 -> result = Gacha.multiPull(bannerFuXuanPull);
+                    case 4 -> result = Gacha.multiPull(bannerBladePull);
                 }
                 panelButton.removeAll();
                 panelButton.add(one);
@@ -231,21 +230,13 @@ public class WindowsBanners extends JFrame implements ActionListener {
 
             }
             if (e.getSource() == back) {
-                reload();
-            }
-            if (e.getSource() == next) {
-                actualConfig++;
-                reload();
-            }
-            if (e.getSource() == previous) {
-                actualConfig--;
-                reload();
+                reload(bannerPromoListe.get(actualConfig));
             }
         }
 
     }
 
-    private void reload() {
+    private void reload(String bannerName) {
         ini = true;
         mainPanel.removeAll();
         ;
@@ -254,42 +245,10 @@ public class WindowsBanners extends JFrame implements ActionListener {
         /* Zone de pull */
         panelPull = new JPanel();
         panelButton = new JPanel();
-        switch (actualConfig) {
-            case 0: // Fenetre Seele
-                bannerSeele = new ImageIcon("Assets/BannerPromo/Seele.jpg");
-                panelButton.add(one);
-                panelButton.add(ten);
-                panelButton.add(next);
-                break;
-            case 1:
-                bannerSeele = new ImageIcon("Assets/BannerPromo/Bronya.jpg");
-                panelButton.add(previous);
-                panelButton.add(one);
-                panelButton.add(ten);
-                panelButton.add(next);
-                break;
-            case 2: // Fenetre Kafka
-                bannerSeele = new ImageIcon("Assets/BannerPromo/Kafka.png");
-                panelButton.add(previous);
-                panelButton.add(one);
-                panelButton.add(ten);
-                panelButton.add(next);
-                break;
-            case 3: // Fenetre Fu Xuan
-                bannerSeele = new ImageIcon("Assets/BannerPromo/Fu_Xuan.png");
-                panelButton.add(previous);
-                panelButton.add(one);
-                panelButton.add(ten);
-                panelButton.add(next);
-                break;
-            case 4:
-                bannerSeele = new ImageIcon("Assets/BannerPromo/Blade.jpg");
-                panelButton.add(previous);
-                panelButton.add(one);
-                panelButton.add(ten);
-                break;
-
-        }
+        bannerSeele = new ImageIcon("Assets/BannerPromo/" + bannerName + ".jpg");
+        panelButton.add(one);
+        panelButton.add(ten);
+        panelButton.add(bannerComboBox);
         tempo = bannerSeele.getImage().getScaledInstance(1280, 600, Image.SCALE_SMOOTH);
         bannerSeele = new ImageIcon(tempo);
         banner = new JLabel(bannerSeele);
